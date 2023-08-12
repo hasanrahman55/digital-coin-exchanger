473fcfd94d1a4b4abfce1cf3c65a16a6077326eb
Hasan Rahman <hasanrahman33605@gmail.com>
2023-08-12 08:26:03 +0600

    navigation bar design

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,8 @@
 // import { HiMenuAlt4 } from "react-icons/hi";
 // import { AiOutlineClose } from "react-icons/ai";
 
-// import logo from "../../images/logo.png";
-
+import React, { useState } from "react";
+import logo from "../../images/logo.png";
 // const NavBarItem = ({ title, classprops }) => (
 //   <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
 // );
@@ -49,23 +49,79 @@
 
 // export default Navbar;
 function NavBar() {
+  const Links = [
+    { name: "Home", link: "/" },
+    { name: "Coin", link: "/coin" },
+
+  ];
+  let [open, setOpen] = useState(false);
+
   return (
-    <nav className="nav">
-      <ul>
-        <li>
-          <a href="/">HomePage</a>
-        </li>
-        <li>
-          <a href="/price">Price</a>
-        </li>
-        <li>
-          <a href="/account">account</a>
-        </li>
-        <li>
-          <a href="/coin">Coin</a>
-        </li>
-      </ul>
-    </nav>
+    <div className="shadow-md w-full  justify-between top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div
+          className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800"
+        >
+          Designer
+        </div>
+
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-20 " : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+              <a
+                href={link.link}
+                className="text-gray-800 hover:text-gray-400 duration-500"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+
+    // <nav className="w-full flex md:justify-center justify-between items-center p-4">
+    //   <div className="md:flex-[0.5] flex-initial justify-center items-center">
+    //     <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+    //   </div>
+    //   <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+    //     {Links.map((link) => (
+    //       <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+    //         <a
+    //           href={link.link}
+    //           className="text-gray-800 hover:text-gray-400 duration-500"
+    //         >
+    //           {link.name}
+    //         </a>
+    //       </li>
+    //     ))}
+    //     <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+    //       Login
+    //     </li>
+    //   </ul>
+    // </nav>
+
+    // <nav className="nav">
+    //   <ul>
+    //     <li>
+    //       <a href="/">HomePage</a>
+    //     </li>
+    //     <li>
+    //       <a href="/price">Price</a>
+    //     </li>
+    //     <li>
+    //       <a href="/account">account</a>
+    //     </li>
+    //     <li>
+    //       <a href="/coin">Coin</a>
+    //     </li>
+    //   </ul>
+    // </nav>
   );
 }
 
