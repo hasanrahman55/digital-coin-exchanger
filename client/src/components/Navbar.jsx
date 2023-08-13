7f5c07479cf0b9ed02688f1f8914f2e7057ab6c3
Hasan Rahman <hasanrahman33605@gmail.com>
2023-08-13 13:06:11 +0600

    ..

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ function NavBar() {
     <div className="shadow-md w-full  justify-between top-0 left-0">
       <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
         <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins]  text-gray-800">
-          Digital Coin Exchanger
+          <a href="/"> Digital Coin Exchanger</a>
         </div>
 
         <ul
